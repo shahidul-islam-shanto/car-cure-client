67ae6b5dd717f88a5508af19659ded35b0ced136
Shahidul Islam <shahidul@gmail.com>
2025-11-16 23:35:44 +0600

    service details page update

--- src/Pages/ServicesDetails/ServicesDetails.jsx
+++ src/Pages/ServicesDetails/ServicesDetails.jsx
@@ -12,12 +12,137 @@ const ServicesDetails = () => {
     <>
       <div className="">
         <BredCrumb />
-        <div className="py-20">
+        <div className="xl:py-[130px] lg:py-[110px] md:py-[90px] sm:py-[70px] py-[60px]">
           <div className="container-2">
             <div className="grid grid-cols-12 gap-6">
               <div className="col-span-8">
                 <div className="">
-                  <img src={img} alt="" />
+                  <div className="mb-12">
+                    <img
+                      className="w-full h-full rounded-xl"
+                      src={img}
+                      alt=""
+                    />
+                  </div>
+                  <div className="">
+                    <div className="mb-[50px]">
+                      <h2 className="text-nu20 font-bold mb-8">
+                        Unique Car Engine Service
+                      </h2>
+                      <p className="text-nu30 mb-[30px]">
+                        There are many variations of passages of Lorem Ipsum
+                        available, but the majority have suffered alteration in
+                        some form, by injected humour, or randomised words which
+                        don't look even slightly believable. If you are going to
+                        use a passage of Lorem Ipsum, you need to be sure there
+                        isn't anything embarrassing hidden in the middle of
+                        text.
+                      </p>
+                      <div className="grid grid-cols-12 gap-6 mb-[30px]">
+                        <div className="col-span-6">
+                          <div className="px-10 py-10 border-t-2 border-primary1 bg-nu70 rounded-xl">
+                            <h5 className="text-nu20 font-bold mb-3">
+                              Instant Car Services
+                            </h5>
+                            <p className="text-nu30 font-medium">
+                              It uses a dictionary of over 200 Latin words,
+                              combined with a model sentence structures.
+                            </p>
+                          </div>
+                        </div>
+                        <div className="col-span-6">
+                          <div className="px-10 py-10 border-t-2 border-primary1 bg-nu70 rounded-xl">
+                            <h5 className="text-nu20 font-bold mb-3">
+                              24/7 Quality Service
+                            </h5>
+                            <p className="text-nu30 font-medium">
+                              It uses a dictionary of over 200 Latin words,
+                              combined with a model sentence structures.
+                            </p>
+                          </div>
+                        </div>
+                        <div className="col-span-6">
+                          <div className="px-10 py-10 border-t-2 border-primary1 bg-nu70 rounded-xl">
+                            <h5 className="text-nu20 font-bold mb-3">
+                              Easy Customer Service
+                            </h5>
+                            <p className="text-nu30 font-medium">
+                              It uses a dictionary of over 200 Latin words,
+                              combined with a model sentence structures.
+                            </p>
+                          </div>
+                        </div>
+                        <div className="col-span-6">
+                          <div className="px-10 py-10 border-t-2 border-primary1 bg-nu70 rounded-xl">
+                            <h5 className="text-nu20 font-bold mb-3">
+                              Quality Cost Service
+                            </h5>
+                            <p className="text-nu30 font-medium">
+                              It uses a dictionary of over 200 Latin words,
+                              combined with a model sentence structures.
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                      <p className="text-nu30 mb-[30px]">
+                        There are many variations of passages of Lorem Ipsum
+                        available, but the majority have suffered alteration in
+                        some form, by injected humour, or randomised words which
+                        don't look even slightly believable. If you are going to
+                        use a passage of Lorem Ipsum, you need to be sure there
+                        isn't anything embarrassing hidden in the middle of
+                        text.{" "}
+                      </p>
+                    </div>
+                    <div className="">
+                      <h2 className="text-nu20 font-bold mb-[30px]">
+                        3 Simple Steps to Process
+                      </h2>
+                      <p className="text-nu30 font-medium mb-[30px]">
+                        There are many variations of passages of Lorem Ipsum
+                        available, but the majority have suffered alteration in
+                        some form, by injected humour, or randomised words which
+                        don't look even slightly believable. If you are going to
+                        use a passage of Lorem Ipsum, you need to be sure there
+                        isn't anything embarrassing hidden in the middle of text
+                      </p>
+                      <div className="grid grid-cols-12 gap-6">
+                        <div className="col-span-4">
+                          <div className="border-t-2 border-nu50 rounded-xl px-6 py-6 text-center">
+                            <div className="px-3 py-3 bg-[#FF38111A] inline-block rounded-full mb-3">
+                              <h5 className="px-3 py-2 bg-primary1 rounded-full text-nu10 font-bold">
+                                01
+                              </h5>
+                            </div>
+                            <h5 className="text-[20px] text-nu20 font-bold mb-3">Step One</h5>
+                            <p className="text-nu30 font-medium">It uses a dictionary of over 200 .</p>
+                          </div>
+                        </div>
+                         <div className="col-span-4">
+                          <div className="border-t-2 border-nu50 rounded-xl px-6 py-6 text-center">
+                            <div className="px-3 py-3 bg-[#FF38111A] inline-block rounded-full mb-3">
+                              <h5 className="px-3 py-2 bg-primary1 rounded-full text-nu10 font-bold">
+                                02
+                              </h5>
+                            </div>
+                            <h5 className="text-[20px] text-nu20 font-bold mb-3">Step Two</h5>
+                            <p className="text-nu30 font-medium">It uses a dictionary of over 200 .</p>
+                          </div>
+                        </div>
+                         <div className="col-span-4">
+                          <div className="border-t-2 border-nu50 rounded-xl px-6 py-6 text-center">
+                            <div className="px-3 py-3 bg-[#FF38111A] inline-block rounded-full mb-3">
+                              <h5 className="px-3 py-2 bg-primary1 rounded-full text-nu10 font-bold">
+                                03
+                              </h5>
+                            </div>
+                            <h5 className="text-[20px] text-nu20 font-bold mb-3">Step Three</h5>
+                            <p className="text-nu30 font-medium">It uses a dictionary of over 200 .</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
