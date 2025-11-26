1ec9bea253ed2e20013e66b742bbc47a6843d9d9
Shahidul Islam <shahidul@gmail.com>
2025-11-27 01:09:25 +0600

    custom hook update

--- src/Components/ServicesSection/ServicesSection.jsx
+++ src/Components/ServicesSection/ServicesSection.jsx
@@ -1,18 +1,21 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import ServiceCard from "../ServiceCard/ServiceCard";
+import useServices from "../../Hooks/useServices";
 
 const ServicesSection = () => {
-  const [services, setServices] = useState([]);
+  // const [services, setServices] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/services")
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/services")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       console.log(data);
 
-        setServices(data);
-      });
-  }, []);
+  //       setServices(data);
+  //     });
+
+  // }, []);
+  const services = useServices();
   return (
     <>
       <div className="pb-20">
@@ -35,7 +38,9 @@ const ServicesSection = () => {
               ))}
             </div>
             <div className="text-center ">
-                <button className="px-6 py-2 rounded-lg border-2 border-primary1 text-primary1">More Services</button>
+              <button className="px-6 py-2 rounded-lg border-2 border-primary1 text-primary1">
+                More Services
+              </button>
             </div>
           </div>
         </div>
